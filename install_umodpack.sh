6f171ce5e1dee176fcc89d67ab49c8a2296204ab
Neil Gentleman <ngentleman@gmail.com>
2015-09-19 15:54:31 -0700

    script to install umodpack
    ancient package needs some fixing up

---
+++ install_umodpack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+wget -N http://archive.kernel.org/debian-archive/debian/pool/main/u/umodpack/umodpack_0.5b16-2_all.deb
+sudo dpkg -i umodpack_0.5b16-2_all.deb
+sudo cpan install Win32::Registry::File
+sudo sed -i 's/Config::Ini/Win32::Registry::File/' /usr/bin/{,x}umod
+sudo sed -i 's/Config::Ini/Win32::Registry::File/' /usr/share/perl5/Umod.pm
