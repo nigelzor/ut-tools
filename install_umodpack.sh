52395d59baaed9b139756123413851bf801fc7f0
Neil Gentleman <ngentleman@gmail.com>
2015-09-19 19:18:21 -0700

    clean up umod warnings

--- install_umodpack.sh
+++ install_umodpack.sh
@@ -6,3 +6,4 @@ sudo dpkg -i umodpack_0.5b16-2_all.deb
 sudo cpan install Win32::Registry::File
 sudo sed -i 's/Config::Ini/Win32::Registry::File/' /usr/bin/{,x}umod
 sudo sed -i 's/Config::Ini/Win32::Registry::File/' /usr/share/perl5/Umod.pm
+sudo sed -i 's/%hash->{/$hash{/' /usr/share/perl5/Umod.pm
